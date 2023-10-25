af28269f98edd13c49bfc69e6273eef4377f32d8
Samuel Gonzalez <sgonza21@uwyo.edu>
2023-10-24 23:19:18 -0600

    initial version of search working, need to remove dead ends from final output

--- code.js
+++ code.js
@@ -1,3 +1,32 @@
+//using adjecency list
+
 function depthFirstSearch(graph, startNode, targetNode) {
-    return [];
+    visited = [startNode];
+    path = [startNode];
+
+    function depthFirstSearchInner(graph, startNode, targetNode) {
+        if(startNode == targetNode) {
+            return startNode;
+        }
+        for (i = 0; i < graph[startNode].length; i++){
+            if(visited.includes(graph[startNode][i] )){
+                continue;
+            }
+            else {
+                visited.push(graph[startNode][i])
+                path.push(graph[startNode][i])
+                depthFirstSearchInner(graph, graph[startNode][i], targetNode )
+            }
+        }
+        //path.pop()
+    }   
+
+    depthFirstSearchInner(graph, startNode, targetNode)
+
+    return path;
 }
+
+
+a = [[0],[3,2],[1],[1,2]]
+depthFirstSearch(a)
+console.log(depthFirstSearch(a,3,2))
