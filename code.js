476d3d57aaa35787f4e3e85f1bd920a5fe37439b
Samuel Gonzalez <sgonza21@uwyo.edu>
2023-12-14 19:08:07 -0700

    moved non automatic test to a test file

--- code.js
+++ code.js
@@ -40,11 +40,3 @@ function depthFirstSearch(graph, startNode, targetNode) {
 
     return finalPath;
 }
-
-a = [[0,1],[3,2],[1],[1,2]]
-b = [[2],[6],[2,3],[3,4],[4,5],[5,6],[6,7],[7,0]]
-console.log(depthFirstSearch(b,0,7))
-console.log(depthFirstSearch(b,1,0))
-console.log(depthFirstSearch(a,3,7))
-console.log(depthFirstSearch(a,3,2))
-console.log(depthFirstSearch(a,0,3))
